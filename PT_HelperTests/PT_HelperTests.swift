b031715f9ecd3f723602a84b2312e19c58287350
Nathaniel Okun <nokun@stanford.edu>
2015-05-10 22:12:00 -0700

    Initial Commit

---
+++ PT_HelperTests/PT_HelperTests.swift
@@ -0,0 +1,36 @@
+//
+//  PT_HelperTests.swift
+//  PT_HelperTests
+//
+//  Created by Nathaniel Okun on 5/10/15.
+//  Copyright (c) 2015 Nathaniel Okun. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PT_HelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
